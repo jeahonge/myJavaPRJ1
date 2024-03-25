5eaabe2a96f6238dd74df97b59f9966a89cdb54a
ParkJeaHong <hong8011e@gmail.com>
2024-03-25 13:33:01 +0900

    2번째 파일

---
+++ src/main/java/study/Class4_9.java
@@ -0,0 +1,11 @@
+package study;
+
+public class Class4_9 {
+    public static void main(String[] args) {
+        System.out.println("\n줄바꿈\n연습");
+        System.out.println(("\t 탭키\t연습"));
+        System.out.println("어떤 글자를\"강조\"하는효과1");
+        System.out.println("어떤 글자를 \'강조\'하는효과2");
+        System.out.println("\\\\ 백슬래시 2개 출력");
+    }
+}
